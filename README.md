da44af3bf85239c10f8ca52539bfdb8688d32c13
Leif Denby <leif@denby.eu>
2017-10-27 12:55:27 +0100

    Ensure correct version installs and add README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Testing execution `jedi` through different versions
+
+This repository represents a quick attempt at studying the changes in execution
+time for [jedi](). The `run_all.sh` is integrated to run with Travis CI and
+[results are available there](https://travis-ci.org/leifdenby/jedi-testing).
--- run.sh
+++ run.sh
@@ -6,10 +6,12 @@ if [ "$#" -lt 1 ]; then
     echo
 else
     pip2 uninstall -y --quiet jedi parso > /dev/null 2>&1
-    pip2 install git+git://github.com/davidhalter/jedi.git@${1}
 
-    if [ "$#" -eq 3 ]; then
-         pip2 install git+git://github.com/davidhalter/parso.git@${1}
+    if [ "$#" -eq 2 ]; then
+         pip2 install git+git://github.com/davidhalter/parso.git@${2}
+         pip2 install --no-deps git+git://github.com/davidhalter/jedi.git@${1}
+    else
+         pip2 install git+git://github.com/davidhalter/jedi.git@${1}
     fi
     echo "First call:"
     python2 test.py
